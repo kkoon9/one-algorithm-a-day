2f82f3e561c129af88132722162517ce37251bdf
kkoon9 <rndrnjs2003@gmail.com>
2022-09-04 21:35:40 +0900

    feat: 백준 1546 kotlin 공부용
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/baekjoon/_1000/_1546_kotlin/main.kt
@@ -0,0 +1,22 @@
+package baekjoon._1000._1546_kotlin
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Integer.max
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var token = StringTokenizer(readLine())
+    val count = token.nextToken().toInt()
+    val scoreOfSubjects = Array(count) { 0 }
+    token = StringTokenizer(readLine())
+    var maxOfSubjects = -1
+    var sumOfSubjects = 0
+    for(i in 0 until count) {
+        scoreOfSubjects[i] = token.nextToken().toInt()
+        sumOfSubjects += scoreOfSubjects[i]
+        maxOfSubjects = max(scoreOfSubjects[i], maxOfSubjects)
+    }
+
+    println((sumOfSubjects.toDouble() / maxOfSubjects) * 100 / count)
+}
