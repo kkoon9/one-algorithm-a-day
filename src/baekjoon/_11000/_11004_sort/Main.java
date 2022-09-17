b525b0d5e0e4ebad069a4f47426c3a51965eb044
kkoon9 <rndrnjs2003@gmail.com>
2022-09-17 14:02:23 +0900

    feat: 백준 11004
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/baekjoon/_11000/_11004_sort/Main.java
@@ -0,0 +1,25 @@
+package baekjoon._11000._11004_sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[K-1]);
+    }
+}
