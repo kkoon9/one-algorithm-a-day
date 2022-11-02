ce76a565788d86afa698663d034e234c0e3f9eee
kkoon9 <rndrnjs2003@gmail.com>
2022-11-02 21:59:41 +0900

    feat: 백준 1717
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/baekjoon/_1000/_1717_union/Main.java
@@ -0,0 +1,62 @@
+package baekjoon._1000._1717_union;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringTokenizer st;
+    private static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        parent = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int question = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (question == 0) {
+                union(a, b);
+            } else {
+                if (checkSame(a, b)) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static boolean checkSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    private static int find(int a) {
+        if (a == parent[a]) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+}
