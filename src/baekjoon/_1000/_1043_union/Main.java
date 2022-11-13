ec7873819bc1f485ace845df1f12766e90f62e2c
kkoon9 <rndrnjs2003@gmail.com>
2022-11-13 21:00:12 +0900

    fix: 2252 수정
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/baekjoon/_1000/_1043_union/Main.java
+++ src/baekjoon/_1000/_1043_union/Main.java
@@ -2,71 +2,47 @@ package baekjoon._1000._1043_union;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     private static StringTokenizer st;
-    private static int[] parent;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        int[][] cities = new int[N + 1][N + 1];
-        parent = new int[N + 1];
-        for (int i = 0; i <= N; i++) {
-            parent[i] = i;
-        }
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                cities[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int[] route = new int[M + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= M; i++) {
-            route[i] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+        int[] indegree = new int[N + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            list.get(s).add(e);
+            indegree[e]++;
         }
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (cities[i][j] == 1) union(i, j);
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 1;i<=N;i++ ){
+            if (indegree[i] ==0) {
+                queue.offer(i);
             }
         }
 
-        int index = find(route[1]);
-        for(int i = 2;i<=M;i++) {
-            if(index != find(route[i])) {
-                System.out.println("NO");
-                return;
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            System.out.print(now + " ");
+
+            for(int next : list.get(now)) {
+                indegree[next]--;
+                if(indegree[next] == 0){
+                    queue.offer(next);
+                }
             }
         }
-        System.out.println("YES");
     }
 
-    private static boolean checkSame(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a == b) {
-            return true;
-        }
-        return false;
-    }
-
-    private static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            parent[b] = a;
-        }
-    }
-
-    private static int find(int a) {
-        if (a == parent[a]) {
-            return a;
-        }
-        return parent[a] = find(parent[a]);
-    }
 }
---
+++ src/baekjoon/_2000/2252_union/Main.java
@@ -0,0 +1,72 @@
+package baekjoon._2000_2252_union;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringTokenizer st;
+    private static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[][] cities = new int[N + 1][N + 1];
+        parent = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                cities[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] route = new int[M + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= M; i++) {
+            route[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (cities[i][j] == 1) union(i, j);
+            }
+        }
+
+        int index = find(route[1]);
+        for(int i = 2;i<=M;i++) {
+            if(index != find(route[i])) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
+
+    private static boolean checkSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    private static int find(int a) {
+        if (a == parent[a]) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+}
